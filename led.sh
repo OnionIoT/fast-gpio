921f35808946bb58ad1290bf0b27e7c0bbf31d66
greenbreakfast <lazardemin@gmail.com>
2015-10-01 17:59:21 +0800

    Modularized the exp led script for testing

--- led.sh
+++ led.sh
@@ -1,5 +1,72 @@
 #!/bin/sh -v
 
+Usage () {
+	echo "Functionality:"
+	echo "	Set colors on Omega Expansion Dock LED"
+	echo ""
+	echo "Usage:"
+	echo "$0 <hex value>"
+	echo "	Displays color of hex value on the Exp Dock LED"
+	echo ""
+	echo "$0 -help"
+	echo "	Displays this prompt"
+	echo ""
+}
+
+# convert hex to duty cycle 
+#	argument 1 - hex value (expecting 0x12 format)
+# 	returns output via echo
+HexToDuty () {
+	# convert hex to decimal
+	decimal=$(($1))
+
+	# find the duty %
+	duty=$(($decimal*100/255))
+
+	#return the duty
+	echo "$duty"
+}
+
+# flip duty cycle polarity
+#	argument 1 - duty cycle (0-100)
+#	returns output via echo
+FlipDutyPolarity () {
+	# flip the polarity
+	ret=$((100-$1))
+
+	# return the new duty
+	echo "$ret"
+}
+
+# performs hex to duty conversion for exp led
+# 	argument 1 - hex value (0x12 format)
+# 	returns duty via exho
+RgbHexToDuty () {
+	# hex to duty
+	duty=$( HexToDuty $1 )
+
+	# flip the duty polarity
+	dutyFlip=$( FlipDutyPolarity $duty )
+
+	# return the duty
+	echo "$dutyFlip"
+}
+
+
+########################
+##### Main Program #####
+
+# check for arguments
+if 	[ $# != 1 ] ||
+	[ "$1" == "-help" ] || 
+	[ "$1" == "-h" ] || 
+	[ "$1" == "--help" ] || 
+	[ "$1" == "--h" ];
+then
+	Usage
+	exit;
+fi
+
 #read color argument
 hex=$1;
 
@@ -24,10 +91,15 @@ rDuty=$((100-$rDuty))
 gDuty=$((100-$gDuty))
 bDuty=$((100-$bDuty))
 
+rDuty2=$(RgbHexToDuty $rHex)
+gDuty2=$(RgbHexToDuty $gHex)
+bDuty2=$(RgbHexToDuty $bHex)
+
 
 echo "Colors: $rHex $gHex $bHex"
 echo "Colors: $rDec $gDec $bDec"
 echo "Colors: $rDuty $gDuty $bDuty"
+echo "Colors: $rDuty2 $gDuty2 $bDuty2"
 
 #run the pwm
 ./fast-gpio pwm 17 200 $rDuty
